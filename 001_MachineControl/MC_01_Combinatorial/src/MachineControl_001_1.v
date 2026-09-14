3f36853cbade2c8c19a8933580e1565b29d5a0cc
savver <savver.engineer@gmail.com>
2026-09-14 16:11:00 +0300

    001_MachineControl \ MC_01_Combinatorial - update

--- 001_MachineControl/MC_01_Combinatorial/src/MachineControl_001_1.v
+++ 001_MachineControl/MC_01_Combinatorial/src/MachineControl_001_1.v
@@ -9,7 +9,9 @@
 на них 0, значит произошле какой-то сбой, и надо выключить 
 все двигатели.
 ПЛИС будет выполнять ряд задач по контролю и индикации.
+
 Задачи ПЛИС:
+
 1) если нет срабатываний аварийных датчиков, и сигналов 
 ошибок с драйверов моторов, то горит зеленая лампочка,
 надо подать сигналы разрешения работы на драйверы моторов.
@@ -17,23 +19,66 @@
 красную лампочку, выключить все драйвера моторов.
 */
 
+  `define USE_ASSIGN_RED_VAR1
+//`define USE_ASSIGN_RED_VAR2
+//`define USE_ASSIGN_RED_VAR3
+
+  `define USE_ASSIGN_GREEN_VAR1
+//`define USE_ASSIGN_GREEN_VAR2
+//`define USE_ASSIGN_GREEN_VAR3
 
 module MachineControl_001_1
 (
-	input [4:0]  MOT_ERR,
-	input [2:0]  FAIL_SENSn,
+	input [4:0]  MOT_ERR,    //positive active, т.е. если 1, то сбой произошел
+	input [2:0]  FAIL_SENSn, //negative active, т.е. если 0, то сбой произошел
 	output [4:0] MOT_ENA,
 	output 	     LED_GREEN,
 	output 	     LED_RED
 );
 
-assign LED_RED   = MOT_ERR[4]    | MOT_ERR[3]    | MOT_ERR[2]   | MOT_ERR[1] | MOT_ERR[0] |
-                   !FAIL_SENSn[2] | !FAIL_SENSn[1] | !FAIL_SENSn[0];
+//---var 1 ----------------
+`ifdef USE_ASSIGN_RED_VAR1
 
-assign LED_GREEN = !MOT_ERR[3]    & !MOT_ERR[2]    & !MOT_ERR[2]  & !MOT_ERR[1] & !MOT_ERR[0] &
-                   FAIL_SENSn[2]  & FAIL_SENSn[1]  & FAIL_SENSn[0] ;
-				   
-//assign LED_GREEN = !LED_RED;
+	assign LED_RED   = MOT_ERR[4]    | MOT_ERR[3]    | MOT_ERR[2]   | MOT_ERR[1] | MOT_ERR[0] |
+							!FAIL_SENSn[2] | !FAIL_SENSn[1] | !FAIL_SENSn[0];
+`endif	
+
+//---var 2 ----------------			 
+`ifdef USE_ASSIGN_RED_VAR2
+	// |MOT_ERR    - OR всех битов MOT_ERR
+	// ~FAIL_SENSn - инверсия всех бит шины
+
+	assign LED_RED   = |MOT_ERR  |  |(~FAIL_SENSn);
+`endif
+
+//---var 3 ----------------
+`ifdef USE_ASSIGN_RED_VAR3
+	//FAIL_SENSn = 111 → ~& = 0   // все датчики OK
+	//FAIL_SENSn = 110 → ~& = 1   // один FAIL
+	//FAIL_SENSn = 101 → ~& = 1
+	//FAIL_SENSn = 011 → ~& = 1
+	//
+	//~FAIL_SENSn[2] | ~FAIL_SENSn[1] | ~FAIL_SENSn[0] <=> ~&FAIL_SENSn
+	
+	assign LED_RED = |MOT_ERR  |  ~&FAIL_SENSn;
+`endif				 
+
+
+//---var 1 ----------------
+`ifdef USE_ASSIGN_GREEN_VAR1
+	assign LED_GREEN = !MOT_ERR[3]    & !MOT_ERR[2]    & !MOT_ERR[2]  & !MOT_ERR[1] & !MOT_ERR[0] &
+							 FAIL_SENSn[2]  & FAIL_SENSn[1]  & FAIL_SENSn[0] ;
+`endif	
+//---var 2 ----------------
+`ifdef USE_ASSIGN_GREEN_VAR2
+
+	assign LED_GREEN = ~|MOT_ERR  &  &FAIL_SENSn;
+`endif	
+//---var 3 ----------------
+`ifdef USE_ASSIGN_GREEN_VAR3		
+	   
+  assign LED_GREEN = !LED_RED;
+`endif	
 
 assign MOT_ENA[4:0] = LED_RED ? 4'b0000 : 4'b1111;
 
